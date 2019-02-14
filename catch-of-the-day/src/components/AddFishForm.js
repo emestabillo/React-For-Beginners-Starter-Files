3da2b9149e60c686a9cdfc1cf52428937d709549
emestabillo <emsestabillo@gmail.com>
2019-02-13 19:29:34 -0500

    Add fish form

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -51,34 +51,55 @@ class AddFishForm extends React.Component {
 
 export default AddFishForm;
 
-// import React, { Fragment } from 'react';
-// import { getFunName } from "../helpers";
+// import React from "react";
 
-// class StorePicker extends React.Component {
-//   myInput = React.createRef();
+// class AddFishForm extends React.Component {
+//   nameRef = React.createRef();
+//   priceRef = React.createRef();
+//   statusRef = React.createRef();
+//   descRef = React.createRef();
+//   imageRef = React.createRef();
 
-//   goToStore = event => { //event object is full of info of what happened
-//     //1. Stop the form from submitting -- (bec this refreshes the page):
+//   createFish = event => {
+//     //1. stop the form from submitting
 //     event.preventDefault();
-//     //2. Get the text from that input
-//     const storeName = this.myInput.current.value;
-//     //3. Change the page to /store/whatever-they-entered
-//     this.props.history.push(`/store/${storeName}`)
+//     const fish = {
+//       name: this.nameRef.current.value,
+//       price: parseFloat(this.priceRef.current.value),
+//       status: this.statusRef.current.value,
+//       desc: this.descRef.current.value,
+//       image: this.imageRef.current.value
+//     };
+//     this.props.addFish(fish);
+//     //refresh the form
+//     event.currentTarget.reset();
 //   };
 //   render() {
 //     return (
-//       <form className="store-selector" onSubmit={this.goToStore}>
-//         <h2>Please Enter A Store</h2>
+//       <form className="fish-edit" onSubmit={this.createFish}>
+//         <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
 //         <input 
-//           type="text"
-//           ref={this.myInput} //will surface the input on the component so we can grab it
-//           required placeholder="Store Name" 
-//           defaultValue={getFunName()}
+//           name="price" 
+//           ref={this.priceRef} 
+//           type="text" 
+//           placeholder="Price" 
+//           />
+//         <select name="status" ref={this.statusRef}>
+//           <option value="available">Fresh!</option>
+//           <option value="unavailable">Sold Out!</option>
+//         </select>
+        
+//         <textarea name="desc" ref={this.descRef} placeholder="Desc" />
+//         <input 
+//           name="image" 
+//           ref={this.imageRef}
+//           type="text" 
+//           placeholder="Image" 
 //         />
-//         <button type="submit">Visit Store -> </button>
+//         <button type ="submit">+ Add Fish</button>
 //       </form>
-//     )
+//     );    
 //   }
 // }
 
-// export default StorePicker;
+// export default AddFishForm;
