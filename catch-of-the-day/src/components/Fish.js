ed1a7f65d313895f51e589aea405e32df1d19fdd
Emmilie Estabillo <emsestabillo@gmail.com>
2019-02-26 22:42:58 -0500

    Add index to fishes

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -3,7 +3,8 @@ import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
   render() {
-    const{ image, name, price, description, status } = this.props.details;
+    const{ image, name, price, desc, status } = this.props.details;
+    const isAvailable = status === "available";
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
@@ -11,6 +12,9 @@ class Fish extends React.Component {
           {name}
           <span className="price">{formatPrice(price)}</span>
         </h3>
+        <p>{desc}</p>
+        <button disabled={!isAvailable} onClick={() => this.props.addToOrder(this.props.index)}>
+          {isAvailable ? "Add To Cart" : "Sold Out"}</button>
       </li>
     );
   }
